d5fae2d21b066b979ed35c0ed76b037b37b56797
github username <github email address>
2017-06-20 17:56:00 -0400

    wot

--- app/tattoo_class.rb
+++ app/tattoo_class.rb
@@ -1,29 +1,40 @@
 require 'csv'
 require 'json'
 require 'sqlite3'
-
+require 'pry'
 
 #data =
 
 #CSV.parse(data).to_json
 
 
-extracted_data   = CSV.table('data/nba-tattoos/nba_tattoos.csv')
-transformed_data = extracted_data.map { |row| row.to_hash } #turns extracted data into hash
 
 
 
 # puts transformed_data
-
+binding.pry
 class CreateTattoo
-  attr_accessor :db
 
+  extracted_data   = CSV.table("./nba-tattoos/nba_tattoos.csv")
+  transformed_data = extracted_data.map { |row| row.to_hash } #turns extracted data into hash
 
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS tattoos (
+  id INTEGER PRIMARY KEY,
+  player_name TEXT,
+  tattoo Boolean)
+  SQL
 
-  def db
-    @db ||= SQLite3::Database.new("data/nba-tattoos/tattoo.db")
-  end
-
+  db = SQLite3::Database.new("./nba-tattoos/tattoos.db")
+  db.execute("DROP TABLE IF EXISTS tattoos;")
   db.execute(sql)
+
+  transformed_data.each do |data|
+    insert_sql = "Insert into tattoos (player_name, tattoo) values (?, ?)"
+    db.execute(insert_sql, data[:player_name], data[:tattoos_yesno])
+  end
+  #
+  db.execute("Select * from tattoos")
+  # db.execute(sql)
 
 end
